09182391a11716886ee1e16754a2c452ffbbd479
Greg Swan <gmswan99@gmail.com>
2022-12-22 16:19:49 -0500

    added views

--- metal_django/metal/views.py
+++ metal_django/metal/views.py
@@ -1,4 +1,24 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import ArtistSerializer, ArtistRigSerializer, RigDetailSerializer
+from .models import Artist, ArtistRig, RigDetail
+
 
 # Create your views here.
-from .models import Artist, ArtistRig, RigDetail
+class ArtistList(generics.ListCreateAPIView):
+    queryset = Artist.objects.all()
+    serializer_class = ArtistSerializer
+
+class ArtistDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Artist.objects.all()
+    serializer_class = ArtistSerializer
+
+class ArtistRig(generics.RetrieveUpdateDestroyAPIView):
+    queryset = ArtistRig.objects.all()
+    serializer_class = ArtistRigSerializer
+
+
+class RigDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = RigDetail.objects.all()
+    serializer_class = RigDetailSerializer
+    
